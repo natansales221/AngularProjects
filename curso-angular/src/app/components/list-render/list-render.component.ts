d93c6f560321ad033d09f752b10b7c69d5fc1f54
natansales221 <natansales221@gmail.com>
2024-07-21 15:55:48 -0300

    input + pipes

--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -21,7 +23,7 @@ export class ListRenderComponent implements OnInit {
     age: 45
   };
 
-  constructor() { }
+  constructor(private listService: ListService) { }
 
   animalDetails = '';
 
@@ -31,5 +33,10 @@ export class ListRenderComponent implements OnInit {
   showAge(animal: Animal) {
     this.animalDetails = `O Pet ${animal.name} tem ${animal.age} anos`;
 
+  }
+
+  removeAnimal(animal: Animal){
+    console.log("removendo animal... ");
+    this.animals = this.listService.remove(this.animals, animal);
   }
 }
